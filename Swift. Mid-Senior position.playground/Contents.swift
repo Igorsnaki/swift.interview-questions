dccaae9a6fa42f3744e3bf6557448b792a051314
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2017-11-05 19:26:19 +0300

    Add some question for Mid-Senior position

---
+++ Swift. Mid-Senior position.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/* What result will be printed?
+
+var foo: UInt8 = 255
+foo += 1
+print("Result is \(foo)")
+
+*/
+
+/* What result will be printed?
+
+var uint32: UInt32 = UInt32.max
+var uint8: UInt8 = UInt8(uint32)
+print("Result is \(uint8)")
+
+*/
+
