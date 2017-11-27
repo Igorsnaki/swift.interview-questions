f27c269e8a7b3a47f6cb938625c1ef566470a306
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2017-11-27 11:38:45 +0300

    Add questions list

---
+++ Swift. Questions. Junior position.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+/*
+* Obj-C. Memory management (retain, release, autorelease, autorelease pool). ARC/Non ARC. Reference counting.
+* Obj-C. Categories/Extensions. Duplicated method in categories.
+* Obj-C. KVC
+* Obj-C. KVO
+* Obj-C. Method dispatching.
+* Obj-C/Swift. Retain cycle.
+* NSThread. Run loop. Event loop.
+* NSTimer. Run loop. Event loop.
+ */
+
+/*
+* UIKit. View/View controller lifecycle. `awakeFromNib`, `initWithCoder`, `initWithFrame`
+* NSOperation. Cancel operation. Dependencies. Promises, Bolts Kit.
+* UIKit. UIStackView.
+ */
+
+/*
+* Delegate pattern vs Observer pattern.
+* iOS Design Patters. MVC, VIPER, MVVM.
+* Dependency injection.
+* CocoaPods, Carthage.
+* White label applications.
+* Push notifications. Certificates, provision profiles.
+* Http request stack.
+ */
+
