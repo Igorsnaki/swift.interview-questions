0b8ad8a74fce18fa0dc658c9772de2297a8baa7b
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2017-12-14 16:18:17 +0300

    Add some questions for Interview

--- Swift. Code. Mid-Senior position.playground/Contents.swift
+++ Swift. Code. Mid-Senior position.playground/Contents.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-/* What result will be printed?
+/* ================================================================
+ * What result will be printed?
 
 var foo: UInt8 = 255
 foo += 1
@@ -8,7 +9,8 @@ print("Result is \(foo)")
 
 */
 
-/* What result will be printed?
+/* ================================================================
+ * What result will be printed?
 
 var uint32: UInt32 = UInt32.max
 var uint8: UInt8 = UInt8(uint32)
@@ -16,3 +18,13 @@ print("Result is \(uint8)")
 
 */
 
+/* ================================================================
+ * What the result of the expression?
+struct Empoyee: NSObject {
+    let name: String?
+}
+
+let workers = [Empoyee(name: "Ivan"), Empoyee(name: "Petr"), Empoyee(name: "Dima"), Empoyee(name: "Alina")]
+let names = workers.value(forKey: "name")
+print("names are: \(names)")
+*/
--- Swift. Questions. Junior position.playground/Contents.swift
+++ Swift. Questions. Junior position.playground/Contents.swift
@@ -1,24 +1,31 @@
 //: Playground - noun: a place where people can play
 
 /*
-* Obj-C. Memory management (retain, release, autorelease, autorelease pool). ARC/Non ARC. Reference counting.
+* Obj-C. Manual Memory management (retain, release, autorelease, autorelease pool). ARC/Non ARC.
+* Retain cycle.
 * Obj-C. Categories/Extensions. Duplicated method in categories.
 * Obj-C. KVC. Issues.
-* Obj-C. KVO. Issues.
+* Obj-C. KVO. Issues. WillChangeValueForKey/DidChangeValueForKey
 * Obj-C. Method dispatching.
-* Obj-C/Swift. Retain cycle.
 * NSThread. Run loop. Event loop.
 * NSTimer. Run loop. Event loop.
  */
 
 /*
- * Swift. Error handling. NSError vs Error.
+* Swift. Class vs structure.
+* Swift. Error handling. NSError vs Error.
+* Swift. Equantable vs Comparable.
+* Swift. KVC issues.
  */
 
 /*
+* Frame vs Bounds.
+* CocoaTouch. Multithreading. NSOperation. GCD. Serial/concarrent queues. Dispatch async/sync.
+* UIKit. CALayer.
 * UIKit. View/View controller lifecycle. `awakeFromNib`, `initWithCoder`, `initWithFrame`
 * NSOperation. Cancel operation. Dependencies. Promises, Bolts Kit.
-* UIKit. UIStackView.
+* UIKit. UIStackView. Size classes.
+* Load and present Big images for thumbnails.
  */
 
 /*
@@ -30,5 +37,8 @@
 * Push notifications. Certificates, provision profiles.
 * Http request stack.
 * Swift vs Objective-C
+* Return optional or throw error?
+* NSTimer retains target?
+* Perform selector after delay?
  */
 
