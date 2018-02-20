c9bda702255c3714c9d02625c238fe112fd39d88
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2018-02-20 17:02:43 +0300

    Update

--- Swift. Questions. Junior position.playground/Contents.swift
+++ Swift. Questions. Junior position.playground/Contents.swift
@@ -1,35 +1,58 @@
 //: Playground - noun: a place where people can play
 
 /*
-* Obj-C. Manual Memory management (retain, release, autorelease, autorelease pool). ARC/Non ARC.
-* Retain cycle.
+* Obj-C. Manual Memory management (retain, release, autorelease). ARC/Non ARC.
+* Obj-C. Stack vs Heap
+* Weak vs Strong vs unowned references. Delegate references type.
+* Retain cycle. Blocks
 * Obj-C. Categories/Extensions. Duplicated method in categories.
 * Obj-C. KVC. Issues.
 * Obj-C. KVO. Issues. WillChangeValueForKey/DidChangeValueForKey
+* Obj-C. Runtime.
 * Obj-C. Method dispatching.
-* NSThread. Run loop. Event loop.
-* NSTimer. Run loop. Event loop.
+* Obj-C. Autorelease pool. Autorelease pool in ARC.
  */
 
 /*
-* Swift. Class vs structure.
+* Swift. Class vs structure. Value type vs Reference type
 * Swift. Error handling. NSError vs Error.
+* Swift. Generics.
+* Swift. Obj-C compatibility.
 * Swift. Equantable vs Comparable.
 * Swift. KVC issues.
  */
 
 /*
-* Frame vs Bounds.
+ * Multithreading and concurency. GCD. NSOperationQueue. Serial/concarrent queues. Dispatch async/sync.
+ * NSThread. Runloop
+ * performSelectorInBackground:withObject:
+ * Async methods synchronization.
+ * NSTimer. Run loop. Event loop.
+ */
+
+/*
+* UIKit. Frame vs Bounds. @1x, @2x, @3x.
+* UIKit. Application states. Background mode.
+* UIKit. CoreGraphics.
+* UIKit. DisplayLink timer.
+* Secure storage on iOS. Keychain.
+* Core data. Uniq ID.
+* UIKit. UITableView/UICollectionView. performBatchUpdates(_:completion:) (2 inserts 3 deletes)
+* UIStackView. Size classes.
+* Constraint-base (autolayout). Perform animation. Change priority. Draw an example to show priority changes!!!
 * Interface Builder. Top/Bottom Layout Guide. Safe area meaning.
-* CocoaTouch. Multithreading. NSOperation. GCD. Serial/concarrent queues. Dispatch async/sync.
-* UIKit. CALayer.
+* UIKit. UIView and CALayer relationships.
 * UIKit. View/View controller lifecycle. `awakeFromNib`, `initWithCoder`, `initWithFrame`
-* NSOperation. Cancel operation. Dependencies. Promises, Bolts Kit.
-* UIKit. UIStackView. Size classes.
 * Load and present Big images for thumbnails.
  */
 
 /*
+ * Instruments. Memory leaks. Zombie objects.
+ */
+
+/*
+* Dependencies. Promises, Bolts Kit.
+* Release procedure. Provision profile, certificate meaning.
 * Delegate pattern vs Observer pattern.
 * Service Locator(SL) vs Dependency Injection(DI)
 * iOS Design Patters. MVC, VIPER, MVVM.
